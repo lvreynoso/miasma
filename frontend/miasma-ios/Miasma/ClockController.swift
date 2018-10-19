fedfc3af5e8907d0c126ea4e3dcf804dfb095dd9
Lucia Reynoso <rvaicul@gmail.com>
2018-10-18 23:31:21 -0700

    ITS THE FINAL COUNTDOWN

--- frontend/miasma-ios/Miasma/ClockController.swift
+++ frontend/miasma-ios/Miasma/ClockController.swift
@@ -11,6 +11,11 @@ import UIKit
 
 class ClockController: UIViewController {
     
+    var deadline: Date?
+    var apocalypse: String?
+    var clockValues: UILabel!
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .black
@@ -20,35 +25,48 @@ class ClockController: UIViewController {
         let safeArea = UIView(frame: bounds)
         // testBoundaries(in: safeArea)
         setupCountdownClock(in: safeArea)
+        setupExplainerLabel(in: safeArea)
         self.view.addSubview(safeArea)
         
-        print("view loaded!")
-
+        startUpdating()
     }
     
-    func testBoundaries(in testView: UIView) {
-        let top = setupClockValueLabel("top")
-        top.sizeToFit()
-        top.center = CGPoint(x: testView.center.x, y: testView.bounds.minY)
-        let bottom = setupClockValueLabel("bottom")
-        bottom.sizeToFit()
-        bottom.center = CGPoint(x: testView.center.x, y: testView.bounds.maxY)
-        let cents = setupClockValueLabel("center")
-        cents.sizeToFit()
-        cents.center = testView.center
-        
-        testView.addSubview(top)
-        testView.addSubview(bottom)
-        testView.addSubview(cents)
-    }
     
     func setupCountdownClock(in clockView: UIView) {
-        let clockValues = setupClockValueLabel("16\n8\n2\n14\n12\n53")
-        clockValues.center = CGPoint(x: clockView.center.x - CGFloat(150), y: (clockValues.bounds.size.height / 2))
+        // access the countdown date
+        guard let deadlineUrl = Bundle.main.url(forResource: "Deadline", withExtension: "plist") else {
+            print("can't find Deadline.plist")
+            return
+        }
+        do {
+            let deadlinePlist = try PropertyListSerialization.propertyList(from: Data(contentsOf: deadlineUrl), options: [], format: nil) as? [String: Any]
+            
+            self.deadline = deadlinePlist?["Point of No Return"] as? Date
+        } catch {
+            print("can't create array from plist")
+            return
+        }
         
+        // where the magic happens
+        if self.deadline != nil {
+            self.apocalypse = Interval.until(self.deadline!)
+        }
+        
+        // create the value labels
+        clockValues = setupClockValueLabel(apocalypse!)
+        clockValues.center = CGPoint(x: clockView.center.x - CGFloat(150), y: (clockValues!.bounds.size.height / 2))
+        
+        // create the ymd-hms labels
         let clockLabels = setupClockLabelText("years\nmonths\ndays\nhours\nminutes\nseconds")
         clockLabels.center = CGPoint(x: clockView.center.x, y: (clockLabels.bounds.size.height / 2))
         
+       
+        
+        clockView.addSubview(clockValues)
+        clockView.addSubview(clockLabels)
+    }
+    
+    func setupExplainerLabel(in clockView: UIView) {
         let explainerLabel = UILabel()
         explainerLabel.text = "until the point of no return."
         explainerLabel.textAlignment = .center
@@ -57,11 +75,7 @@ class ClockController: UIViewController {
         explainerLabel.sizeToFit()
         explainerLabel.center = CGPoint(x: clockView.center.x, y: clockView.center.y)
         
-        clockView.addSubview(clockValues)
-        clockView.addSubview(clockLabels)
         clockView.addSubview(explainerLabel)
-        
-        print("labels added!")
     }
     
     func setupClockValueLabel(_ text: String) -> UILabel {
@@ -88,4 +102,14 @@ class ClockController: UIViewController {
         return returnLabel
     }
     
+    func startUpdating() {
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: {_ in
+                guard let deadline = self.deadline else { return }
+                let updatedApocalypse = Interval.until(deadline)
+                self.clockValues.text = updatedApocalypse
+            })
+        }
+    }
+
 }
---
+++ frontend/miasma-ios/Miasma/Interval.swift
@@ -0,0 +1,171 @@
+//
+//  Interval.swift
+//  Miasma
+//
+//  Created by Lucia Reynoso on 10/18/18.
+//  Copyright © 2018 Lucia Reynoso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Interval {
+    
+    static func until(_ deadline: Date) -> String {
+        var interval: String = ""
+        var year: Int = 2035
+        var years: Int = 0
+        var months: Int = 0
+        var days: Int = 0
+        var hours: Int = 0
+        var minutes: Int = 0
+        var seconds: Int = 0
+        
+        // interval in seconds
+        let now = Date()
+        var timeInterval = Int(deadline.timeIntervalSince(now))
+        
+        let commonYear: Int = 31536000
+        let leapYear: Int = 31622400
+        
+        struct Months {
+            let thirtyOne: Int = 2678400
+            let thirty: Int = 2592000
+            let twentyNine: Int = 2505600
+            let twentyEight: Int = 2419200
+        }
+        
+        let monthSeconds = Months.init()
+        
+        var timeIsMoreThanOneYear: Bool = false
+        var timeIsMoreThanOneMonth: Bool = false
+        
+        // first we whittle down the years
+        // 2034 & 2035 are common years
+        if timeInterval > commonYear {
+            timeIsMoreThanOneYear = true
+        }
+        
+        while timeIsMoreThanOneYear {
+            switch year {
+            case 2020, 2024, 2028, 2032:
+                if timeInterval > leapYear {
+                    timeInterval -= leapYear
+                    years += 1
+                    year -= 1
+                } else {
+                    timeIsMoreThanOneYear = false
+                }
+            default:
+                if timeInterval > commonYear {
+                    timeInterval -= commonYear
+                    years += 1
+                    year -= 1
+                } else {
+                    timeIsMoreThanOneYear = false
+                }
+            }
+        }
+        
+        // then we whittle down the months
+        let monthGetter = DateFormatter()
+        monthGetter.dateFormat = "MM"
+        
+        let currentMonth = Int(monthGetter.string(from: now))!
+        
+        // check if the time remaining is more than what's in next month
+        var nextMonth = (currentMonth + 1) % 12
+        switch nextMonth {
+        case 01, 03, 05, 07, 08, 10, 12:
+            if timeInterval > monthSeconds.thirtyOne {
+                timeIsMoreThanOneMonth = true
+            }
+        case 04, 06, 09, 11:
+            if timeInterval > monthSeconds.thirty {
+                timeIsMoreThanOneMonth = true
+            }
+        case 02:
+            switch year {
+            case 2020, 2024, 2028, 2032:
+                if timeInterval > monthSeconds.twentyNine {
+                    timeIsMoreThanOneMonth = true
+                }
+            default:
+                if timeInterval > monthSeconds.twentyEight {
+                    timeIsMoreThanOneMonth = true
+                }
+            }
+        default:
+            if timeInterval > monthSeconds.twentyEight {
+                timeIsMoreThanOneMonth = true
+            }
+        }
+        
+        // whittle it down
+        while timeIsMoreThanOneMonth {
+            switch nextMonth {
+            case 01, 03, 05, 07, 08, 10, 12:
+                if timeInterval > monthSeconds.thirtyOne {
+                   timeInterval -= monthSeconds.thirtyOne
+                    months += 1
+                    nextMonth = (nextMonth + 1) % 12
+                } else {
+                    timeIsMoreThanOneMonth = false
+                }
+            case 04, 06, 09, 11:
+                if timeInterval > monthSeconds.thirty {
+                    timeInterval -= monthSeconds.thirty
+                    months += 1
+                    nextMonth = (nextMonth + 1) % 12
+                } else {
+                    timeIsMoreThanOneMonth = false
+                }
+            case 02:
+                switch year {
+                case 2020, 2024, 2028, 2032:
+                    if timeInterval > monthSeconds.twentyNine {
+                        timeInterval -= monthSeconds.twentyNine
+                        months += 1
+                        nextMonth = (nextMonth + 1) % 12
+                    } else {
+                        timeIsMoreThanOneMonth = false
+                    }
+                default:
+                    if timeInterval > monthSeconds.twentyEight {
+                        timeInterval -= monthSeconds.twentyEight
+                        months += 1
+                        nextMonth = (nextMonth + 1) % 12
+                    } else {
+                        timeIsMoreThanOneMonth = false
+                    }
+                }
+            default:
+                if timeInterval > monthSeconds.twentyEight {
+                    timeInterval -= monthSeconds.twentyEight
+                    months += 1
+                    nextMonth = (nextMonth + 1) % 12
+                } else {
+                    timeIsMoreThanOneMonth = false
+                }
+            }
+        }
+        
+        // now to take care of any remaining days, hours, minutes, and seconds
+        
+        days = timeInterval / 86400
+        timeInterval -= days * 86400
+        
+        hours = timeInterval / 3600
+        timeInterval -= hours * 3600
+        
+        minutes = timeInterval / 60
+        timeInterval -= minutes * 60
+        
+        seconds = timeInterval
+        
+        interval = "\(years)\n\(months)\n\(days)\n\(hours)\n\(minutes)\n\(seconds)"
+        
+        return interval
+    }
+    
+}
